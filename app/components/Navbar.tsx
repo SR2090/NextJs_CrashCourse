9debba3f09a7bd7a51a46bd983b89c4581f9a3ea
SR2090 <myworkemailid2557@gmail.com>
2024-11-16 16:44:27 +0530

    navbar component without using server actions

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { auth, signIn, signOut } from "@/auth"
+const Navbar = async () => {
+    const session = await auth();
+    return <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+        <nav className='flex justify-between items-center'>
+            <Link href="/">
+                <Image src="/MainLogo.png" width={144} height={10} alt='logo' />
+            </Link>
+            <div className='flex items-center gap-5'>
+                {
+                    session && session?.user ? (<>
+                        <Link href='/startup/create' className='text-black'>
+                            <span>Create</span>
+                        </Link>
+                        <button onClick={async () => { await signOut() }} className='text-red-500'>LogOut</button>
+                        <Link href={`/user/${session?.user?.id}`} ><span>{session?.user?.name}</span></Link>
 
-const Navbar = () => {
-    return <div className='w-full bg-red-400'>Nav</div>
+                    </>) : <>
+                        {/* Provide a github */}
+                        <button onClick={async () => { await signIn('github') }} className='text-red-500'>LogIn</button>
+                    </>
+                }
+            </div>
+        </nav>
+    </header>
 }
 
 export default Navbar
+
+
+// <Image src={session?.user?.image} alt='user-image' width={30} height={30} className='rounded-full' />
