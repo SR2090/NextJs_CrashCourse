9daecb0cc04def1eae70ff68346a0b50489ec890
SR2090 <myworkemailid2557@gmail.com>
2024-11-16 16:44:12 +0530

    Added navbar to layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
+import Navbar from "../components/Navbar";
+
 const Layout = ({ children }: { children: React.ReactNode }): React.JSX.Element => {
     return <>
+        <Navbar />
         <main>
             {children}
         </main>
