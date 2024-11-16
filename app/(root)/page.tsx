f7151e65797f655b35e30755695137d0deb7530e
SR2090 <myworkemailid2557@gmail.com>
2024-11-17 00:26:06 +0530

    Added pink section to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,10 @@
 const Home = (): React.JSX.Element => {
   return (
-    <div className="heading">Home Page</div>
+    <><section className="pink_container">
+      <div className="heading">Pitch your Startup, <br /> Connect With Entrepreneurs</div>
+      ; <p className="sub-heading !max-w-3xl">Submit ideas vote on pitches and get noticed on virtual competitions</p>
+    </section></>
+
   );
 }
 export default Home;
