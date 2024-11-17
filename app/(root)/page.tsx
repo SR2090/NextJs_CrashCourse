49dd493d5f72731b3583dee1a2ad8e0348656cee
SR2090 <myworkemailid2557@gmail.com>
2024-11-17 12:07:50 +0530

    added search form section to home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+import SearchForm from "../components/SearchForm";
+
 const Home = (): React.JSX.Element => {
   return (
     <><section className="pink_container">
       <div className="heading">Pitch your Startup, <br /> Connect With Entrepreneurs</div>
       ; <p className="sub-heading !max-w-3xl">Submit ideas vote on pitches and get noticed on virtual competitions</p>
-    </section></>
+      <SearchForm />
+    </section>
+
+    </>
 
   );
 }
